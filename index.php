b30f82b54c566f5d7f4a0d7a3a1cfd88eba198c9
Denismaka <makadenis370@gmail.com>
2025-04-03 21:09:01 +0100

    first commit et mise en place du Header responsive

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/font/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.min.css">
+    <link rel="website icon" href="/assets/images/GroceryMart_Logo.webp" />
+    <title>GroceryMart</title>
+</head>
+
+<body>
+    <!-- Bouton de redirection -->
+    <div class="arrow">
+        <i class="fas fa-arrow-up icone"></i>
+    </div>
+
+    <!-- ############## -->
+    <!-- Header -->
+    <!-- ############## -->
+    <header>
+        <a href="index.php" class="logo">
+            <img src="/assets/images/GroceryMart_Logo.webp" class="img-fluid rounded">
+            GroceryMart
+        </a>
+        <div class="menu-toggle" id="mobile-menu">
+            <i class="fas fa-bars"></i>
+        </div>
+        <ul class="nav-list" id="nav-list">
+            <li><a href="/index.php#home" class="text-capitalize">home</a></li>
+            <li><a href="/index.php#about" class="text-capitalize">about</a></li>
+            <li><a href="/index.php#products" class="text-capitalize">products</a></li>
+            <li><a href="/index.php#pages" class="text-capitalize">pages</a></li>
+            <li><a href="/index.php#contact" class="text-capitalize">contact</a></li>
+        </ul>
+    </header>
+
+
+    <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/Js/app.js"></script>
+</body>
+
+</html>
