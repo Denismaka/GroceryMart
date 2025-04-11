ad88a5b08b0c7477a83caec55efdb28f776ebe0b
Denismaka <makadenis370@gmail.com>
2025-04-11 14:04:04 +0100

    Mise en place du script J =s pour la gestion du menu burger, pour la gestion du bouton de redirection, ainsi que du scrool de la page

---
+++ assets/Js/app.js
@@ -0,0 +1,24 @@
+// Bouton de redirection
+const btn = document.querySelector(".arrow");
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Gestion du menu burger
+const mobileMenu = document.getElementById("mobile-menu");
+const navList = document.getElementById("nav-list");
+
+mobileMenu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+
+// JavaScript pour les effets de la barre de navigation lors du défilement
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
