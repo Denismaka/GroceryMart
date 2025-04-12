6f152d185d694daa5478ab3d5f83324d6076e7a9
Denismaka <makadenis370@gmail.com>
2025-04-12 17:26:10 +0100

    Ajout du Script ScrollReveal pour les animations au défilement

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -22,3 +22,15 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Script ScrollReveal pour les animations au défilement
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".container, .heading", { origin: "top" });
+ScrollReveal().reveal(".col-md-4, form", { origin: "top" });
+ScrollReveal().reveal("h1, h3, h4, h5, h2", { origin: "top" });
+ScrollReveal().reveal("p, .row, .arrow", { origin: "top" });
